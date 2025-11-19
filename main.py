a3eedba6841bc775e9d82897bf1374fcf4ba9b31
Z0mbie001 <94248812+Z0mbie001@users.noreply.github.com>
2025-11-19 21:28:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ## Barcode Scanner
-## Written in Python 3.9.13
+## Written in Python 3.12.4, it is compatabale with any Python version after Python 3.10
 ## Created and Licensed by Jack Babey
 
 ## Import Statements
@@ -10,28 +10,81 @@ import json
 def main():
     ##catalogue = readCatFile()
     ##writeCatFile(catalogue)
-    menuChoice = outputMenu()
-    match menuChoice:
-        case 1:
-            purchase()
-        case 2:
-            refill()
-        case 3:
-            barcodeValidation()
-        case _:
-            print("UNKNOWN ERROR HAS OCCURED")
-    return None
+    clearScreen()
+    while True:
+        menuChoice = outputMenu()
+        match menuChoice:
+            case 1:
+                purchase()
+            case 2:
+                restock()
+            case 3:
+                barcodeValidation()
+            case 4:
+                return None
+            case _:
+                clearScreen()
+                print("UNKNOWN ERROR HAS OCCURED")
+ 
+## Menu Choices
+# Purchasing Items
+def purchase():
+    print("Please scan or enter the barcode of the item you would like to purchase")
+    barcode = input(">> ")
+    if(barcode.isnumeric() != True):
+        clearScreen()
+        print("Submitted Barcode Must Be In Numeric Format")
+        return None
+
+    item = getItem(barcode)
+    if(item == None):
+        clearScreen()
+        print("Item Doesn't Exist")
+        return None
     
+    print("There are " + item["Count"] + " left in stock")
+    print("Please enter the quantity desired")
+    count = input(">> ")
+    if(count.isnumeric() != True):
+        clearScreen()
+        print("The Item Count Must be an Integer Number")
+        return None
+    elif(count > item["Count"]):
+        clearScreen()
+        print("There isn't enough stock of that item")
+        return None
+
+# Restock Items
+def restock():
+    return None
+
+# Barcode Validation
+def barcodeValidation():
+    return None
+
+## Item Manipulaton
+def getItem(barcode):
+    data = readCatFile()
+    print(data)
+    print(json.dumps(data))
+    for i in data["Items"]:
+        print(json.dumps(i))
+        if(i["Barcode"] == barcode):
+            return i
+    return None
+
+## Menu   
 def outputMenu():
     print("""
           Please Choose From the Following Options:
           1) Buy A Product
-          2) Refill Stock
+          2) Restock Stock
           3) Barcode Validation
+          4) Exit
           """)
     choice = input(">> ")
     if(validateMenuChoice(choice) == True):
-        return choice
+        return int(choice)
     else:
         outputMenu()
         
@@ -40,13 +93,14 @@ def validateMenuChoice(value):
         clearScreen()
         print("Please enter an integer value")
         return False
-    elif(int(value) > 3 or int(value) < 1):
+    elif(int(value) > 4 or int(value) < 1):
         clearScreen()
         print("Please choose a valid menu option")
         return False
     else:
         return True
-    
+
+## File Manipulation  
 def writeCatFile(toWrite):
     with open("catalogue.json", mode="w", encoding="utf-8") as catFile:
         json.dump(toWrite, catFile)
@@ -57,6 +111,7 @@ def readCatFile():
         catData = json.load(catFile)
     return catData
 
+## Screen Clearing
 def clearScreen():
     for i in range(0, 100):
         print("\n")
